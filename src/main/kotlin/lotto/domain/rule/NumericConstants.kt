629b84dd050a45280f7f95be18188adb011bc96e
chanho0908 <uni070@naver.com>
2024-11-29 22:43:12 +0900

    feat : 정수형 상수 관리 enum class cnrk

--- src/main/kotlin/lotto/domain/rule/NumericConstants.kt
+++ src/main/kotlin/lotto/domain/rule/NumericConstants.kt
@@ -1,5 +1,8 @@
 package lotto.domain.rule
 
 enum class NumericConstants(val value: Int) {
-    ONE_THOUSAND_UNIT(1000)
+    ONE_THOUSAND_UNIT(1000),
+    LOTTO_MIN(1),
+    LOTTO_MAX(45),
+    MAX_WINNING_SIZE(6)
 }
