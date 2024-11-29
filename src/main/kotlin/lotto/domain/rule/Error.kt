9df90a2e8684647873ce568fcebfa9e17321a17d
chanho0908 <uni070@naver.com>
2024-11-29 22:43:24 +0900

    feat : 에러 메시지 상수 관리 클래스 추가

--- src/main/kotlin/lotto/domain/rule/Error.kt
+++ src/main/kotlin/lotto/domain/rule/Error.kt
@@ -2,10 +2,11 @@ package lotto.domain.rule
 
 enum class Error(private val msg: String) {
     EMPTY("빈 값이 입력 되었어요."),
-    DUPLICATED("당첨 번호는 중복될 수 없어요."),
-    INVALID_INPUT("잘못된 입력입니다. 이름은 한/영/숫자로 2명 이상 입력해주세요."),
+    WINNING_DUPLICATED("당첨 번호는 중복될 수 없어요."),
     ONLY_DIGIT("0 이상의 정수만 입력해주세요."),
-    NOT_THOUSAND_UNIT("1000원 단위로 입력해주세요");
+    NOT_THOUSAND_UNIT("1000원 단위로 입력해주세요"),
+    INVALID_RANGE("로또 번호는 1~45 사이의 숫자로 입력해주세요."),
+    INVALID_WINNING_SIZE("당첨 번호는 6개를 입력해주세요");
 
     override fun toString(): String = "$ERROR $msg"
 
